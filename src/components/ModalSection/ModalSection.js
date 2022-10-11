089705bc6a4514ae98810557c10fce891a31e3a4
bhagyashreept <55779449+bhagyashreept@users.noreply.github.com>
2022-10-11 22:27:12 +0530

    1 file modified

--- src/components/ModalSection/ModalSection.js
+++ src/components/ModalSection/ModalSection.js
@@ -2,11 +2,9 @@ import React, { useContext } from 'react';
 import App from './components/src/App.js';
 
 const ModalSection = () => {
-  const user = useContext(UserContext);
   return;
-  <UserContext.Consumer>
-    <h1>{user}</h1>
-  </UserContext.Consumer>;
+
+  <h1>{user}</h1>;
 };
 
 export default ModalSection;
